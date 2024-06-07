8de23288cd8f630f2ec301e4a27ab06c1351b78b
chengdengdeng <hjh1881@163.com>
2024-06-07 16:18:59 +0800

    Add files via upload

---
+++ 135.分发糖果.cpp
@@ -0,0 +1,53 @@
+// 135.分发糖果
+
+//1 两次遍历
+class Solution{
+public:
+	int candy(vector<int>& ratings){
+		int n = ratings.size();
+		vector<int> left(n);
+		for (int i = 0; i < n; ++i) {
+			if (i > 0 && ratings[i] > ratings[i-1]){
+				left[i] = left[i-1] + 1;
+			} else{
+				left[i] = 1;
+			}
+		}
+		int right = 0,ret = 0;
+		for (int i = n-1; i >= 0; i--) {
+			if (i < n-1 && ratings[i] > ratings[i+1]){
+				right++;
+			} else{
+				right = 1;
+			}
+			ret += max(left[i],right);
+		}
+		return ret;
+	}
+};
+
+//常数空间遍历
+class Solution{
+public:
+	int candy(vector<int>& ratings){
+		int n = ratings.size();
+		int ret = 1;
+		int inc = 1,dec = 0,pre = 1;
+		for (int i = 1; i < n; ++i) {
+			if(ratings[i] >= ratings[i-1]){
+				dec = 0;
+				pre = ratings[i] == ratings[i-1] ? 1:pre + 1;
+				ret += pre;
+				inc = pre;
+			} else{
+				dec++;
+				if(dec == inc){
+					dec++;
+				}
+				ret += dec;
+				pre = 1;
+			}
+		}
+		return ret;
+	}
+};
