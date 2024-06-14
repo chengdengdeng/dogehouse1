26ae51ffb9cc6bdf0807c34a04282e5b3be2d120
chengdengdeng <hjh1881@163.com>
2024-06-14 14:34:29 +0800

    Add files via upload

---
+++ 452.引爆气球.cpp
@@ -0,0 +1,22 @@
+// 452.引爆气球
+
+class Solution{
+public:
+	int findMinArrowShots(vector<vector<int>>& points){
+		if (points.empty()){
+			return 0;
+		}
+		sort(points.begin(),points.end(),[](const vector<int>& u,const vector<int>& v){
+			return u[1] < v[1];
+		});
+		int pos = points[0][1];
+		int ans = 1;
+		for (const vector<int>& balloon : points) {
+			if (balloon[0] > pos){
+				pos = balloon[1];
+				ans++;
+			}
+		}
+		return ans;
+	}
+};
