8090996b06ce3c2c18a7da70f3cac3fded4d7e9a
chengdengdeng <hjh1881@163.com>
2024-06-26 20:45:02 +0800

    Add files via upload

---
+++ 46.全排列.cpp
@@ -0,0 +1,22 @@
+// 46.全排列
+
+class Solution{
+public:
+	void backtrack(vector<vector<int>>& res,vector<int>& output,int first,int len){
+		if (first == len){
+			res.emplace_back(output);
+			return;
+		}
+		for (int i = first; i < len; ++i) {
+			swap(output[i],output[first]);
+			backtrack(res,output,first+1,len);
+			swap(output[i],output[first]);
+		}
+	}
+
+	vector<vector<int>> premute(vector<int>& nums){
+		vector<vector<int>> res;
+		backtrack(res,nums,0,(int)nums.size());
+		return res;
+	}
+};
